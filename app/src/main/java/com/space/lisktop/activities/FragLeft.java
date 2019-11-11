55c6fe164425dbe2ea83ea81777765aa39f60b35
HanZhizhi <1486702746@qq.com>
2019-11-11 16:22:02 +0800

    bugFix & motto
    解决不删除主页应用重复添加的bug；可设置主页语句

--- app/src/main/java/com/space/lisktop/activities/FragLeft.java
+++ app/src/main/java/com/space/lisktop/activities/FragLeft.java
@@ -1,11 +1,14 @@
 package com.space.lisktop.activities;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.preference.PreferenceManager;
 
 import android.util.DisplayMetrics;
 import android.util.Log;
@@ -19,17 +22,22 @@ import android.widget.TextView;
 import com.space.lisktop.R;
 import com.space.lisktop.obj.AppInfo;
 import com.space.lisktop.utility.LisktopDAO;
+import com.space.lisktop.utility.PackageManageHelper;
 
 import java.util.ArrayList;
 
 public class FragLeft extends Fragment implements View.OnClickListener {
-    private TextView tvPhone,tvMessage,tvCam,tvWeb,tvSettings;
+    private TextView tvSettings,tvMotto;
     private LisktopDAO lisktopDAO;
     private LinearLayout layoutMainApps;
+    private ArrayList<AppInfo> mainApps;
+    private PackageManager packMan;
+    SharedPreferences sPref;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        packMan=getActivity().getPackageManager();
         Log.i("fd","oncreate");
     }
 
@@ -50,15 +58,9 @@ public class FragLeft extends Fragment implements View.OnClickListener {
         layoutMainApps=rootV.findViewById(R.id.llayout_mainApps);
         layoutMainApps.setOrientation(LinearLayout.HORIZONTAL);
 
-        tvPhone=rootV.findViewById(R.id.tvPhoneCall);
-        tvMessage=rootV.findViewById(R.id.tvMessage);
-        tvCam=rootV.findViewById(R.id.tvCamera);
-        tvWeb=rootV.findViewById(R.id.tvBroswer);
-
-        tvPhone.setOnClickListener(this);
-        tvMessage.setOnClickListener(this);
-        tvCam.setOnClickListener(this);
-        tvWeb.setOnClickListener(this);
+        tvMotto=rootV.findViewById(R.id.left_motto);
+        sPref= PreferenceManager.getDefaultSharedPreferences(getActivity());
+        tvMotto.setText(sPref.getString("motto","error!"));
     }
 
     @Override
@@ -70,6 +72,8 @@ public class FragLeft extends Fragment implements View.OnClickListener {
 
     @Override
     public void onResume() {
+        tvMotto.setText(sPref.getString("motto","error!"));
+
         lisktopDAO =new LisktopDAO(getActivity());
         //无选择的主页应用，跳至选择页面
         if (!lisktopDAO.isDataExist())
@@ -78,16 +82,29 @@ public class FragLeft extends Fragment implements View.OnClickListener {
             startActivity(chsIntent);
         }
         else {      //有选择的主页应用，查找数据库并显示
-            ArrayList<AppInfo> mainApps=lisktopDAO.getMainApps();
-            for (AppInfo ai:mainApps){
-                Log.i("mainApp",ai.getAppName());
-                addMainApp(ai);
+            mainApps=lisktopDAO.getMainApps();
+            //删除原有图标添加新的
+            Log.i("childs",layoutMainApps.getChildCount()+"");
+            int numChilds=layoutMainApps.getChildCount();
+            for (int i=0;i<numChilds;i++)      //原本将getChildCount写在循环中，导致循环上届改变，子view未删完
+            {
+                Log.i("todeleteview",i+"--"+layoutMainApps.getChildAt(0).getId());
+                layoutMainApps.removeView(layoutMainApps.getChildAt(0));           //原为getChildAt(i)，改为一直删除第0个
+            }
+            for (int i=0;i<mainApps.size();i++){
+                //Log.i("mainApp",ai.getAppName());
+                addMainApp(mainApps.get(i),i);
             }
         }
         super.onResume();
     }
 
-    private void addMainApp(AppInfo aInfo)
+    /**
+     *
+     * @param aInfo:添加的appInfo
+     * @param tag 设置给图标的tag，传入为appInfo在数组中的索引
+     */
+    private void addMainApp(AppInfo aInfo,int tag)
     {
         ImageView iv=new ImageView(getActivity());
         iv.setImageDrawable(aInfo.getAppIcon());
@@ -105,6 +122,7 @@ public class FragLeft extends Fragment implements View.OnClickListener {
         params1.leftMargin = 8;
         params1.rightMargin=8;
 
+        iv.setTag(tag);
         iv.setOnClickListener(this);
         layoutMainApps.addView(iv,params1);
     }
@@ -116,26 +134,12 @@ public class FragLeft extends Fragment implements View.OnClickListener {
             case R.id.tvSettings:
                 startActivity(new Intent(getActivity(),SettingsActivity.class));
                 break;
-            case R.id.tvPhoneCall:
-//                SharedPreferences sPref= PreferenceManager.getDefaultSharedPreferences(getActivity());
-//                Log.i("leftshowicon","show:"+sPref.getBoolean("showicon",false));
-                startActivity(new Intent(getActivity(),SettingsActivity.class));
-                break;
-            case R.id.tvCamera:
-                bottomAction("com.lge.camera");
-                break;
-            case R.id.tvBroswer:
-                bottomAction("com.android.chrome");
-                break;
             default:
+                int idx=(int)v.getTag();
+                Intent go=packMan.getLaunchIntentForPackage(mainApps.get(idx).getPackageName());
+                startActivity(go);
                 break;
         }
     }
 
-    //底部按钮功能
-    private void bottomAction(String packName)
-    {
-        Intent intent=getContext().getPackageManager().getLaunchIntentForPackage(packName);
-        startActivity(intent);
-    }
 }
